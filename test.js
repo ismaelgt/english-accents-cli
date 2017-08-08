744d52f78874b26cb91874c8ee35bc2bdf9c9275
Ismael González Trujillo <ismael.gonzaleztrujillo@gmail.com>
2017-08-08 21:18:12 +0100

    Add test for CLI

--- test.js
+++ test.js
@@ -1,5 +1,11 @@
 import test from 'ava'
+import execa from 'execa'
 
-test('foo', t => {
-  t.pass()
+const packageJson = require('./package.json')
+
+const cli = (args, opts) => execa('./cli.js', args, opts)
+
+test('CLI shows package version', async t => {
+  const thing = await cli(['--version'])
+  t.is(thing.stdout, packageJson.version)
 })
